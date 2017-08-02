04deab514f58866ae99f71c2a327e1b9f282fe16
Shane Orend <shane@Shanes-MBP.attlocal.net>
2017-08-02 19:17:45 -0400

    Almost done way easier then Hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var numberofWins = 0
+var numberoflosses = 0
+
+var guessesLeft = 10
+// var guessLetters = document.getElementById('guessLetters')
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var userGuess = []
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var random = computerGuess[Math.floor(Math.random() * computerGuess.length - 1)];
+    console.log(random);
+    
+    if (userGuess === random ) { numberofWins ++;
+    } else if (userGuess != random) {guessesLeft = guessesLeft -1;}
+   
+   if(guessesLeft === -1) {alert("You lost");}
+    
+    var guessLetteres = "<p>You chose: " + userGuess + "</p>";
+    var wins = "<p>Wins: " + numberofWins + "</p>";
+    var losses = "<p>Losses: " + numberoflosses + "</p>";
+    var guess = "<p>Guess Left: " + guessesLeft + "</p>";
+    document.querySelector("#guessLetteres").innerHTML = guessLetteres;
+    document.querySelector("#wins").innerHTML = wins;
+    document.querySelector("#losses").innerHTML = losses;
+    document.querySelector("#guessAmount").innerHTML = guess;
+};
